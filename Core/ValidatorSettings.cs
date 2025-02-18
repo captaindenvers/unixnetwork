29eee68472b1d03ab7a39bf28157496a772b9230
N8kit <nekitsos2006@gmail.com>
2025-02-19 00:11:16 +0300

    Added summary for ValidatorSettings
    This commit summarized everything in this file
    ValidatorSettings();
    PresetsValidatorSettings;
    LoginSettings();
    PasswordSettings();

--- Core/ValidatorSettings.cs
+++ Core/ValidatorSettings.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace UnixLauncher.Core
 {
+    /// <summary>
+    /// Основные настройки подтверждения данных
+    /// </summary>
     internal class ValidatorSettings(char[] AllowedSymbols, int MaxLenght, int MinLenght)
     {
         public char[] AllowedSymbols { get; init; } = AllowedSymbols;
@@ -13,15 +16,25 @@ namespace UnixLauncher.Core
         public int MinLenght { get; init; } = MinLenght;
     }
 
+    /// <summary>
+    /// Заданные значения настроек для полей ввода данных
+    /// </summary>
     internal class PresetsValidatorSettings
     {
+        // Массивы допустимых символов для проверки данных
         private static readonly char[] _allowedLoginSymbols =
             "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_".ToCharArray();
-
         private static readonly char[] _allowedPasswordSymbols =
             "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_!@#$%^&*()-+=<>?/\\|[]{}".ToCharArray();
 
+        /// <summary>
+        /// Готовые настройки логина с максимальной длиной 16 и минимальной 3
+        /// </summary>
         public static ValidatorSettings LoginSettings { get; } = new(_allowedLoginSymbols, 16, 3);
+        
+        /// <summary>
+        /// Готовые настройки пароля с максимальной длиной 32 и минимальной 7
+        /// </summary>
         public static ValidatorSettings PasswordSettings { get; } = new(_allowedPasswordSymbols, 32, 7);
     }
 }

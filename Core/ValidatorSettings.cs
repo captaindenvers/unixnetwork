e9e98b522811a4721662ea9fe2ef19e854dc5eea
ImRibka <101900380+RealRibka@users.noreply.github.com>
2025-02-28 20:44:29 +0300

    Безопасность и минорные улучшения в валидаторах
    Добавлен особенная ошибка для валидатора.
    
    Изменено название переменной на адекватное (lenght на length)
    
    Проверка передаваемых данных в настройках валидатора

--- Core/ValidatorSettings.cs
+++ Core/ValidatorSettings.cs
@@ -1,19 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using UnixLauncher.Core.Exceptions;
 
 namespace UnixLauncher.Core
 {
-    internal class ValidatorSettings(char[] AllowedSymbols, int MaxLenght, int MinLenght)
+    
+    internal class ValidatorSettings
     {
-        public char[] AllowedSymbols { get; init; } = AllowedSymbols;
-        public int MaxLenght { get; init; } = MaxLenght;
-        public int MinLenght { get; init; } = MinLenght;
+        public char[] AllowedSymbols { get; init; }
+        public int MaxLength { get; init; }
+        public int MinLength { get; init; }
+
+        /// <exception cref="ValidatorException">Выбрасывается при недопустимых значениях параметров.</exception>
+        public ValidatorSettings(char[] allowedSymbols, int maxLength, int minLength)
+        {
+            // --- Проверки
+            if (allowedSymbols == null)
+                throw new ValidatorException("Allowed symbols array must not be null.", int.MinValue);
+
+            if (allowedSymbols.Length < 1)
+                throw new ValidatorException("There must be at least 1 allowed symbol.", allowedSymbols.Length);
+
+            if (maxLength < 1)
+                throw new ValidatorException("Max length lower 1.", maxLength);
+
+            if (minLength < 0)
+                throw new ValidatorException("Min length lower 0.", minLength);
+
+            if (minLength > maxLength)
+                throw new ValidatorException($"MinLength ({minLength}) bigger maxLength ({maxLength}).", int.MinValue);
+            
+
+            // --- Инициализация
+            this.AllowedSymbols = allowedSymbols;
+            this.MaxLength = maxLength;
+            this.MinLength = minLength;
+        }
     }
 
-    internal class PresetsValidatorSettings
+    internal static class PresetsValidatorSettings
     {
         private static readonly char[] _allowedLoginSymbols =
             "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_".ToCharArray();
